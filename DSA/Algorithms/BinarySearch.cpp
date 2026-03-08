1f13f857e3c1c5c53c9ed13291db27fee0cf129c
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-03-08 21:34:13 +0500

    code

---
+++ DSA/Algorithms/BinarySearch.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+    int targetIndex(vector<int>& nums,int target)
+    {
+        if(nums.size()==0) return -1;
+        // Binary Search Algorithm
+        int i=0;
+        int j=nums.size()-1;
+        int mid;
+        while(i<=j)
+        {
+            mid=i+(j-i)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]<target) i=mid+1;
+            if(nums[mid]>target) j=mid-1;
+        }
+        return -1;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> nums;
+    for(int i=0;i<100;i++) nums.push_back(i+1);
+    cout<<s.targetIndex(nums,50);
+    return 0;
+}
