a9190fa420b6d5a692136bdba7a2d621af0519bd
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-03-08 23:44:51 +0500

    code

---
+++ DSA/Algorithms/Sorting/bubbleSort.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+    void bubbleSort(vector<int>& nums)
+    {
+        bool isSwapped;
+        for(int i=0;i<nums.size();i++)
+        {
+            isSwapped=false;
+            for(int j=0;j<nums.size()-1-i;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    swap(nums[j],nums[j+1]);
+                    isSwapped=true;
+                }
+            }
+            if(!isSwapped) break;
+        }
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> nums[3]={{2,0,4,5,1,3},{2,1,2},{13,4,51,2,6,73,7,8}};
+    for(int i=0;i<3;i++)
+    {
+        s.bubbleSort(nums[i]);
+        for(int j:nums[i]) cout<<j<<" ";
+        cout<<'\n';
+    }
+    return 0;
+    return 0;
+}
