bc1ad8be7dc9e0fa0f10a606bd5a81161dec02ad
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-03-09 22:41:29 +0500

    code

--- DSA/List/list.cpp
+++ DSA/List/list.cpp
@@ -11,14 +11,18 @@ int main()
 {
     // Declaration:
     list<int> lis;
-    // Major Functions:
+    // Push Back Function:
     lis.push_back(2); // adds element to end of list
     lis.push_back(3);
+    // Push Front Function:
     lis.push_front(1); // adds element to start of list
     lis.push_front(0);
+    // List Print Loop:
     for(int i:lis) cout<<i<<" ";
     cout<<'\n';
+    // Pop Back Function:
     lis.pop_back(); // removes element from end of list
+    // Pop Front Function:
     lis.pop_front(); // removes element from start of list
     for(int i:lis) cout<<i<<" ";
     cout<<'\n';
