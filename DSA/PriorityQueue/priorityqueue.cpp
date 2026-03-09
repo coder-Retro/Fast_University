d96dc125ff00a864b7073b46c45337a0e7ed40d7
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-03-09 23:05:50 +0500

    code

---
+++ DSA/PriorityQueue/priorityqueue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<queue>
+/*
+Priority Queue although is called a queue but under the hood,
+it basically works using the principals of minHeap/maxHeap so
+we can say that it is basically a complete binary tree in the
+disguise.
+*/
+int main()
+{
+    // Declaration:
+    priority_queue<int> pq;
+    return 0;
+}
