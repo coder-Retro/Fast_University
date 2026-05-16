6370c9253bb79b7407aa648362d17aa448d9962a
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-05-17 00:25:44 +0500

    recursive binary search

---
+++ DSA/Techniques/Recursion/PracticeProblems/RecursiveBinarySearch.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+// Recursion Class
+class Recursion
+{
+public:
+    int BinarySearch(const vector<int>& vec,int start,int end,int target)  
+    {
+        if(start>end) return -1;
+        int mid=start+(end-start)/2;
+        if(vec[mid]==target) return mid;
+        if(vec[mid]<target) return BinarySearch(vec,mid+1,end,target);
+        return BinarySearch(vec,start,mid-1,target);
+    }
+};
+// Main Function
+int main()
+{
+    Recursion r;
+    vector<int> vec={1,2,3,4,5,6,7,8,9,10};
+    int target=5;
+    cout<<target<<" found at index : "<<r.BinarySearch(vec,0,vec.size(),target);
+    return 0;
+}
